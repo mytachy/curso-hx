2119ba8fa2a1b26767fba8a50588f8c4008ff5f6
Ramon <ramonfontes11@gmail.com>
2016-11-28 09:50:46 -0200

    ultimos comits da primeira parte

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,18 @@
 var gulp = require('gulp');
-var sass = require('gulp-ruby-sass');
-
+var sass = require('gulp-sass');
+var watch = require ('gulp-watch');
+//task para o sass
 gulp.task('sass', function () {
-    return sass('sass/style.sass').pipe(gulp.dest('css'))
+    return gulp.src('sass/**/*.sass')
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(gulp.dest('css'));
+});
+
+//task para o watch
+gulp.task('watch', function () {
+    gulp.watch('sass/**/*.sass', ['sass']);
 });
 
 
 // taask deafault gulp
-gulp.task('default', function(){
-    console.log('Ola coders');
-});
+gulp.task('default', ['sass', 'watch']);
